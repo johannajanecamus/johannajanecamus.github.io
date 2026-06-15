dd9712899c3b73422a9772f9d5c81555e68f6dca
Johanna <j.camus.546014@umindanao.edu.ph>
2026-06-15 21:28:07 +0800

    fix: remove subpath, configure as user pages site

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { StrictMode } from "react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/johannaj.github.io">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
