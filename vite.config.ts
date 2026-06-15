47d5cb2fff3b5e84d158e85551437627f0d386ad
Johanna <j.camus.546014@umindanao.edu.ph>
2026-06-15 21:13:28 +0800

    we

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/",
+  base: "/johannaj.github.io/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
